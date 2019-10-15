690857af262dc8d98b2d300411e537ab60023db7
Johann Medina <jmedinap@gmail.com>
2019-10-15 09:34:53 -0500

    initial version

---
+++ FaceChecker.cs
@@ -0,0 +1,92 @@
+﻿using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutlookInvitePrevention
+{
+    static class FaceChecker
+    {
+        public static bool GuessIfIAmInFrontOfMyLaptop()
+        {
+            try
+            {
+                string photoFile = ShootPhoto();
+                return CompareWithMe(photoFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Horrible failure= " + ex);
+                return false;
+            }
+        }
+
+
+        private static string ShootPhoto()
+        {
+            string fileName = string.Format(@"D:\Temporal\{0}.png", DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss"));
+            using (VideoCapture capture = new VideoCapture(0))
+            {
+                //capture.Set(CaptureProperty.XI_LedMode, 0);
+                //capture.Set(CaptureProperty.Settings, 1);
+
+                capture.Open(0);
+                if (capture.IsOpened())
+                {
+                    Mat frame = new Mat();
+                    capture.Read(frame);
+                    var image = BitmapConverter.ToBitmap(frame);
+                    image.Save(fileName, ImageFormat.Png);
+                }
+            }
+            return fileName;
+        }
+
+
+        private static bool CompareWithMe(string photoFile)
+        {
+            // look for indexed face, but only the bigger face on the photo
+            float similarityThreshold = 90F;
+            String collectionId = "InvitePreventCollection";
+
+            AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient();
+            Image image = new Image { Bytes = new MemoryStream(File.ReadAllBytes(photoFile)) };
+
+            SearchFacesByImageRequest searchFacesByImageRequest = new SearchFacesByImageRequest()
+            {
+                CollectionId = collectionId,
+                Image = image,
+                FaceMatchThreshold = similarityThreshold,
+                MaxFaces = 2
+            };
+
+            SearchFacesByImageResponse searchFacesByImageResponse = rekognitionClient.SearchFacesByImage(searchFacesByImageRequest);
+            if (searchFacesByImageResponse.FaceMatches?.Count > 0) return true;
+
+            // if not indexed, then compare my photo with all the faces
+            string myPhotoFile = @"D:\Usuarios\johamepl\Downloads\bluepages.png";
+
+            Image myImage = new Image { Bytes = new MemoryStream(File.ReadAllBytes(myPhotoFile)) };
+
+            CompareFacesRequest compareFacesRequest = new CompareFacesRequest()
+            {
+                SourceImage = myImage,
+                TargetImage = image,
+                SimilarityThreshold = similarityThreshold
+            };
+
+            CompareFacesResponse compareFacesResponse = rekognitionClient.CompareFaces(compareFacesRequest);
+            if (compareFacesResponse.FaceMatches?.Count > 0) return true;
+
+            // not myself
+            return false;
+        }
+    }
+}
---
+++ ThisAddIn.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+namespace OutlookInvitePrevention
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            Application.ItemSend += Application_ItemSend;
+        }
+
+        private void Application_ItemSend(object Item, ref bool Cancel)
+        {
+            Outlook.MailItem mail = Item as Outlook.MailItem;
+            if (mail == null) return;
+
+            string content = $"{mail.Subject} {mail.Body}";
+            bool isAnInviteMail = Regex.IsMatch(content, @"invit[a-zÀ-ÿ0-9]*", RegexOptions.IgnoreCase);
+            if (!isAnInviteMail) return;
+
+            bool authorizedToInvite = FaceChecker.GuessIfIAmInFrontOfMyLaptop();
+            if (!authorizedToInvite)
+            {
+                Cancel = true;
+                LockWorkStation();
+            }
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Nota: Outlook ya no genera este evento. Si tiene código que 
+            //    se debe ejecutar cuando Outlook se apaga, consulte https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool LockWorkStation();
+
+        #region Código generado por VSTO
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
